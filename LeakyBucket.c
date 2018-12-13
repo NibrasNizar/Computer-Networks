c3800187da950125a5445b4ca17857ce66410cb9
zubedooo <34791155+zubedooo@users.noreply.github.com>
2018-12-13 20:17:43 +0530

    Update LeakyBucket.c

--- LeakyBucket.c
+++ LeakyBucket.c
@@ -9,28 +9,16 @@ int main(){
         printf("Enter the incoming packet size : ");
         scanf("%d", &incoming);
         printf("Incoming packet size %d\n", incoming);
-	
-        if (incoming <= (buck_size)){
+        if (incoming <= (buck_size - store)){
             store += incoming;
             printf("Bucket buffer size %d out of %d\n", store, buck_size);
         } else {
-            printf("Dropped as size greater than that of bucket %d",buck_size);
-           
-            store = 0;
+            printf("Dropped %d no of packets\n", incoming - (buck_size - store));
+            printf("Bucket buffer size %d out of %d\n", store, buck_size);
+            store = buck_size;
         }
-store = store - outgoing;
-	while(store>0)
-{
-        
-        printf("After outgoing %d,%d packets left out of %d\n",outgoing, store, incoming);
-store = store - outgoing;}
-store=store+outgoing;
-if(store !=0)
-{
-printf("After outgoing %d,%d packets left out of %d\n", store,0, incoming);
-store=0;
-}
-printf("now remaining: %d",store);
+        store = store - outgoing;
+        printf("After outgoing %d packets left out of %d in buffer\n", store, buck_size);
         n--;
     }
 }
